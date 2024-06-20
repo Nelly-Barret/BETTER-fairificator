0a0148cb26994855beea9115c2d3a873cfd16c53
Nelly Barret <34347677+Nelly-Barret@users.noreply.github.com>
2024-06-20 09:54:05 +0200

    clinical and phenotypic variables were inverted (#72)

--- src/utils/ExaminationCategory.py
+++ src/utils/ExaminationCategory.py
@@ -4,5 +4,5 @@ from src.utils.Ontologies import Ontologies
 
 
 class ExaminationCategory(Enum):
-    CATEGORY_CLINICAL = (Ontologies.LOINC.value["url"], "81259-4", "Associated phenotype")
-    CATEGORY_PHENOTYPIC = (Ontologies.LOINC.value["url"], "75321-0", "Clinical finding")
+    CATEGORY_PHENOTYPIC = (Ontologies.LOINC.value["url"], "81259-4", "Associated phenotype")
+    CATEGORY_CLINICAL = (Ontologies.LOINC.value["url"], "75321-0", "Clinical finding")
