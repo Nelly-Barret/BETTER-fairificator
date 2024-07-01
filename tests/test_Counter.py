4853cf737018ae49e34604c990f2f4ef44dd0a8c
Nelly Barret <nelly.barret@inria.fr>
2024-07-01 10:57:45 +0200

    moved to pytest to avoid module paths problems

--- tests/test_Counter.py
+++ tests/test_Counter.py
@@ -1,36 +1,34 @@
-import unittest
-
 from utils.Counter import Counter
 
 
-class TestCounter(unittest.TestCase):
+class TestCounter:
     def test_constructor(self):
         counter = Counter()
-        self.assertEqual(counter.resource_id, 0)
+        assert counter.resource_id == 0
 
     def test_incr_simple(self):
         counter = Counter()
         counter.increment()
-        self.assertEqual(counter.resource_id, 1)
+        assert counter.resource_id == 1
 
     def test_incr_multiple(self):
         counter = Counter()
         for _ in range(10):
             counter.increment()
-        self.assertEqual(counter.resource_id, 10)
+        assert counter.resource_id == 10
 
     def test_set(self):
         counter = Counter()
         counter.set(new_value=100)
-        self.assertEqual(counter.resource_id, 100)
+        assert counter.resource_id == 100
 
         counter.increment()
-        self.assertEqual(counter.resource_id, 101)
+        assert counter.resource_id == 101
 
     def test_reset(self):
         counter = Counter()
         counter.increment()
         counter.increment()
-        self.assertEqual(counter.resource_id, 2)
+        assert counter.resource_id == 2
         counter.reset()
-        self.assertEqual(counter.resource_id, 0)
+        assert counter.resource_id == 0
